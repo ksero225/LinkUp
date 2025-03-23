cc3ad9f5353f200f5ecdbbe5d880c64bde9c3603
Paweł Michalski <pmpawel126@gmail.com>
2025-03-23 19:31:52 +0100

    Messages can now be sent between two users.

--- BACKEND/src/main/java/com/LinkUp/LinkUp/controllers/ChatController.java
+++ BACKEND/src/main/java/com/LinkUp/LinkUp/controllers/ChatController.java
@@ -11,17 +11,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-    SimpMessagingTemplate messagingTemplate;
 
-    public ChatController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.private")
     public void sendPrivateMessage(@Payload Message message) {
-        System.out.println("MESSAGE RECIPIENT: " + message.getRecipient());
-        System.out.println("MESSAGE SENDER: " + message.getSender());
 
-        messagingTemplate.convertAndSendToUser(message.getRecipient(), "/queue/messages", message);
+        messagingTemplate.convertAndSendToUser(message.getRecipient(), "/private", message);
     }
 }
